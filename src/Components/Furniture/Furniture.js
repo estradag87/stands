a978538819a53dd7e687664380ac22f5bcd41723
Daniel <estradag87@gmail.com>
2020-06-09 20:31:38 -0500

    hook - redux -v2

--- src/Components/Furniture/Furniture.js
+++ src/Components/Furniture/Furniture.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Furniture.scss";
 import { Row, Col, Button, Form } from "react-bootstrap";
 import Categories from "../Categories/Categories";
 import { FURNITURES } from "../../constants";
+import { connect } from "react-redux";
 
-class Furniture extends React.Component {
-  render() {
-    // console.log(this.props.furnitures, this.props.selectedCategories);
-    return (
+const Area = ({ FURNITURES, selectedCategories, furnitures }) => (
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    furnitures: FURNITURES 
+  });
+
+// class Furniture extends React.Component {
+//   render() {
+//     // console.log(this.props.furnitures, this.props.selectedCategories);
+//     return (
       <div clasName="section4">
         <div className="showFurnitureList">
           <h3 className="categorydisplay">{this.props.categoryChoosen}</h3>
           <ul>
             <Row className="justify-content-lg-center">
-              {Object.keys(this.props.furnitures)
+              {Object.keys(furnitures)
                 .filter((furniture) => {
                   console.log(furniture);
                   return (
-                    this.props.selectedCategories &&
-                    this.props.selectedCategories.includes(
-                      this.props.furnitures[furniture].category
+                    selectedCategories &&
+                    selectedCategories.includes(
+                      furnitures[furniture].category
                     )
                   );
                 })
@@ -27,27 +36,23 @@ class Furniture extends React.Component {
                   <Col lg={3} md={3} sm={12} xs={12}>
                     <li
                       className="showFurniture"
-                      key={this.props.furnitures[item].id}
+                      key={furnitures[item].id}
                     >
                       <div className="showImage">
                         <img
                           className="image"
-                          src={this.props.furnitures[item].image}
+                          src={furnitures[item].image}
                         />
                       </div>
                       <div className="name">
-                        {this.props.furnitures[item].name}
+                        {furnitures[item].name}
                       </div>
                       <div className="price">
-                        ${this.props.furnitures[item].price} USD
+                        ${furnitures[item].price} USD
                       </div>
                       <button
                         onClick={() =>
-                          this.props.AddToFurnituresResume(
-                            this.props.furnitures[item].id
-                          )
-                        }
-                      >
+                          setCount(count + 1)}>
                         +
                       </button>
                       <div className="quantity">
@@ -70,7 +75,21 @@ class Furniture extends React.Component {
         </div>
       </div>
     );
-  }
-}
+//   }
+// }
+const mapStateToProps = (state) => ({
+  furnitures: state.furnitures,
+  FURNITURES: state.FURNITURES,
+});
 
-export default Furniture;
+const mapDispatchToProps = (dispatch) => ({
+  chooseAre(area) {
+    dispatch({
+      type: "CHOOSE_AREA",
+      area,
+    });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Furniture);
+
